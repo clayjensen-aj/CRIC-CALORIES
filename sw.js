e29cccd4586d026998d482a3a76bd909bb85510c
clayjensen-aj <anubhavkjhagmh@gmail.com>
2026-04-22 04:21:00 +0530

    Modify service worker install log and fetch handler
    Updated console log message and removed fetch handler.

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+  console.log('Service Worker Installed');
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  // Required for PWA status
 });
